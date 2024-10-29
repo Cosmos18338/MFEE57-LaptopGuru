bb02ee71033995fca53084097b41a8b742ec8ed9
Oss <cosmos18338@gmail.com>
2024-10-29 11:08:17 +0800

    補上後端的.env.template，.env會被git ignore

---
+++ frontend/pages/test.js
@@ -0,0 +1,12 @@
+'use client'
+
+import Coupon from '@/components/coupon'
+import React, { useState, useEffect } from 'react'
+
+export default function Test(props) {
+  return (
+    <>
+      <Coupon />
+    </>
+  )
+}
