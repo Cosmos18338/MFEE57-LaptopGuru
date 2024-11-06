3dc5945448cf756f3d4c07838a4e6c830f1c8841
kuma <tingweiz58@gmail.com>
2024-11-06 09:33:16 +0800

    切版修改

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -6,6 +6,8 @@ import LeftAside from '@/components/dashboard/leftAside'
 import CardExample from '@/components/bootstrap/cards'
 import { useAuth } from '@/hooks/use-auth'
 import { useRouter } from 'next/router'
+// 優惠卷
+import Coupon from '@/components/coupon'
 
 export default function Dashboard() {
   const { auth } = useAuth() // 從 auth context 獲取用戶資訊
@@ -42,54 +44,106 @@ export default function Dashboard() {
 
   // 可以添加載入中的狀態顯示
 
-  return (
-    <div className="container">
-      <div className="row d-flex justify-content-center">
-        <LeftAside />
-        <Tabs
-          defaultActiveKey="home"
-          id="uncontrolled-tab-example"
-          className={`mb-5 col-md-9 $primary`}
-          fill
-        >
-          <Tab className="align-items-center" eventKey="home" title="會員中心">
-            <div>
-              <FileManagementTable />
-            </div>
-          </Tab>
-          <Tab eventKey="shopping-record" title="購買清單">
-            <div>
-              <h4>購買清單components\bootstrap\cards.js</h4>
-              <CardExample />
-            </div>
-          </Tab>
-          <Tab eventKey="lease-record" title="租賃清單">
-            <div className="col-6">
-              <h4>Link Tab Content</h4>
-              <p>這裡是另一個連結對應的內容。</p>
-            </div>
-          </Tab>
-          <Tab eventKey="coupon-record" title="優惠券">
-            <div>
-              <h4>Link Tab Content</h4>
-              <p>這裡是另一個連結對應的內容。</p>
-            </div>
-          </Tab>
-          <Tab eventKey="blog-record" title="文章">
-            <div>
-              <h4>Link Tab Content</h4>
-              <p>這裡是另一個連結對應的內容。</p>
-            </div>
-          </Tab>
-          <Tab eventKey="activity-record" title="活動">
-            <div>
-              <h4>Link Tab Content</h4>
-              <p>這裡是活動。</p>
-            </div>
-          </Tab>
-          <Tab eventKey="group-record" title="揪團"></Tab>
-        </Tabs>
-      </div>
+//   return (
+//     <div className="container">
+//       <div className="row d-flex justify-content-center">
+//         <LeftAside />
+//         <Tabs
+//           defaultActiveKey="home"
+//           id="uncontrolled-tab-example"
+//           className={`mb-5 col-md-9 $primary`}
+//           fill
+//         >
+//           <Tab className="align-items-center" eventKey="home" title="會員中心">
+//             <div>
+//               <FileManagementTable />
+//             </div>
+//           </Tab>
+//           <Tab eventKey="shopping-record" title="購買清單">
+//             <div>
+//               <h4>購買清單components\bootstrap\cards.js</h4>
+//               <CardExample />
+//             </div>
+//           </Tab>
+//           <Tab eventKey="lease-record" title="租賃清單">
+//             <div className="col-6">
+//               <h4>Link Tab Content</h4>
+//               <p>這裡是另一個連結對應的內容。</p>
+//             </div>
+//           </Tab>
+//           <Tab eventKey="coupon-record" title="優惠券">
+//             <div>
+//               {/* <h4>Link Tab Content</h4> */}
+//               <p><Coupon/></p>
+//             </div>
+//           </Tab>
+//           <Tab eventKey="blog-record" title="文章">
+//             <div>
+//               <h4>Link Tab Content</h4>
+//               <p>這裡是另一個連結對應的內容。</p>
+//             </div>
+//           </Tab>
+//           <Tab eventKey="activity-record" title="活動">
+//             <div>
+//               <h4>Link Tab Content</h4>
+//               <p>這裡是活動。</p>
+//             </div>
+//           </Tab>
+//           <Tab eventKey="group-record" title="揪團"></Tab>
+//         </Tabs>
+//       </div>
+//     </div>
+//   )
+// }
+
+return (
+  <div className="container">
+    <div className="row d-flex justify-content-center">
+      <LeftAside />
+      <Tabs
+        defaultActiveKey="home"
+        id="uncontrolled-tab-example"
+        className={`mb-5 col-md-9 $primary`}
+        fill
+      >
+        <Tab className="align-items-center" eventKey="home" title="會員中心">
+          <div>
+            <FileManagementTable />
+          </div>
+        </Tab>
+        <Tab eventKey="shopping-record" title="購買清單">
+          <div>
+            <h4>購買清單components\bootstrap\cards.js</h4>
+            <CardExample />
+          </div>
+        </Tab>
+        <Tab eventKey="lease-record" title="租賃清單">
+          <div className="col-6">
+            <h4>Link Tab Content</h4>
+            <p>這裡是另一個連結對應的內容。</p>
+          </div>
+        </Tab>
+        <Tab eventKey="coupon-record" title="優惠券">
+          <div>
+            {/* <h4>Link Tab Content</h4> */}
+            <p><Coupon/></p>
+          </div>
+        </Tab>
+        <Tab eventKey="blog-record" title="文章">
+          <div>
+            <h4>Link Tab Content</h4>
+            <p>這裡是另一個連結對應的內容。</p>
+          </div>
+        </Tab>
+        <Tab eventKey="activity-record" title="活動">
+          <div>
+            <h4>Link Tab Content</h4>
+            <p>這裡是活動。</p>
+          </div>
+        </Tab>
+        <Tab eventKey="group-record" title="揪團"></Tab>
+      </Tabs>
     </div>
-  )
+  </div>
+)
 }
