8bfadb7a5aff200cee49ab8ce4f4ab447736ee31
LoboFreeman <lobofreeman4294@gmail.com>
2024-11-06 16:50:30 +0800

    商品卡片按鈕效果撰寫&&細節頁後端撰寫&&移動商品圖到product資料夾

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -20,4 +20,30 @@ router.get('/card/:product_id', async (req, res) => {
   }
 })
 
+// 取得單一產品詳細資料和圖片路徑
+router.get('/:product_id', async (req, res) => {
+  try {
+    const product_detail = await db.query(
+      'SELECT * FROM product WHERE product_id = ?',
+      req.params.product_id
+    )
+    const product_img = await db.query(
+      'SELECT product_img_path FROM product_img WHERE img_product_id = ?',
+      req.params.product_id
+    )
+    const product_detail_img = await db.query(
+      'SELECT product_img_path FROM product_detail_img WHERE img_product_id = ?',
+      req.params.product_id
+    )
+    const product = { ...product_detail, product_img, product_detail_img }
+    if (!product_detail) {
+      return res.json({ status: 'error', message: '找不到產品' })
+    }
+    return res.json({ status: 'success', data: { product } })
+  } catch (error) {
+    console.error('取得產品失敗:', error)
+    return res.json({ status: 'error', message: '取得產品失敗' })
+  }
+})
+
 export default router
