205bb7231b2766fc5839b3adf9adfe7da37e2f91
Abby <aintluminate@gmail.com>
2024-11-08 00:29:22 +0800

    style(tabs): 調整登入、註冊頁標籤 hover 效果

--- frontend/pages/member/login.js
+++ frontend/pages/member/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from '@/styles/signUpForm.module.scss'
+import Swal from 'sweetalert2'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useAuth } from '@/hooks/use-auth'
@@ -7,19 +8,19 @@ import { MdOutlineEmail, MdLockOutline, MdArrowForward } from 'react-icons/md'
 import { useJumpingLetters } from '@/hooks/jumping-letters-hook'
 
 export default function LogIn(props) {
+  const [showpassword, setShowpassword] = useState(false)
   const { renderJumpingText } = useJumpingLetters()
   const router = useRouter()
-  const { auth, login } = useAuth()
+  const { login } = useAuth()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errors, setErrors] = useState({ error: ' ' })
-
 
   // 以上還是不太確定為什麼需要用狀態管理。登入頁不就送出帳密比對主要這功能就好了嗎？
   const handleSubmit = async (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
-    
+
     try {
       const response = await fetch(`http://localhost:3005/api/login`, {
         method: 'POST',
@@ -39,16 +40,21 @@ export default function LogIn(props) {
         // 可以添加登入成功後的導向
 
         router.push('/dashboard')
-      }
-       else{
+      } else {
         // 處理錯誤情況
         setErrors({
-          message: result.message
+          message: result.message,
         })
       }
     } catch (error) {
       console.error('無法取得資料:', error)
-      alert('登入失敗')
+      Swal.fire({
+        title: '登入失敗',
+        text: '連接伺服器有問題',
+        icon: 'error',
+        confirmButtonText: '確定',
+        confirmButtonColor: '#3085d6',
+      })
     }
   }
 
@@ -66,19 +72,19 @@ export default function LogIn(props) {
         </div>
         <div className={`${styles.right} col`}>
           <div className={`${styles.tabs} d-flex justify-content-between`}>
-            <h7
-              className={`text-white ${styles.hover} link-opacity-50-hover underline-opacity-0 `}
+            <Link
+              className={` ${styles.hover} text-decoration-none text-white`}
+              href="/member/login"
             >
-              <Link className="text-decoration-none" href="/member/login">
-                Log in
-              </Link>
-            </h7>
-            <h7 className="text-white">|</h7>
-            <h7 className={`text-white ${styles.hover}`}>
-              <Link className="text-decoration-none" href="/member/signup">
-                Sign up
-              </Link>
-            </h7>
+              登入Log in
+            </Link>
+            <span className="text-white">|</span>
+            <Link
+              className={`${styles.hover} text-decoration-none text-white`}
+              href="/member/signup"
+            >
+              註冊Sign Up
+            </Link>
           </div>
           <form
             className="mt-5 position-relative d-flex justify-content-center align-items-center"
@@ -86,10 +92,7 @@ export default function LogIn(props) {
           >
             <div className="inputs position-relative">
               <div className="position-relative">
-                <label
-                  htmlFor="email"
-                  className={`form-label text-white ${styles['custom-label']}`}
-                >
+                <label htmlFor="email" className={`form-label text-white`}>
                   帳號(信箱)
                 </label>
                 <input
@@ -98,7 +101,7 @@ export default function LogIn(props) {
                   onChange={(e) => {
                     setEmail(e.target.value)
                   }}
-                  className={`form-control ${styles['custom-input']} `}
+                  className={`form-control ${styles.inputs}`}
                   name="email"
                   required // 添加必填
                 />
@@ -110,33 +113,35 @@ export default function LogIn(props) {
               </div>
 
               <div className="position-relative">
-                <label
-                  htmlFor="password"
-                  className={`form-label text-white ${styles['custom-label']} `}
-                >
+                <label htmlFor="password" className={`form-label text-white`}>
                   密碼
                 </label>
                 <input
-                  type="password"
+                  type={showpassword ? 'text' : 'password'}
                   value={password}
                   autocomplete="new-password"
-                  onChange={(e) => {
-                    setPassword(e.target.value)
+                  onChange={(e)=>{setPassword(e.target.value)}}
+                  onClick={() => {
+                    setShowpassword((prev) => {
+                      console.log('changing showpassword to:', !prev) // 檢查狀態更新
+                      return !prev
+                    })
                   }}
                   id="password"
                   name="password" // 添加 name
-                  className={`form-control ${styles['custom-input']}`}
-                  aria-describedby="passwordHelpBlock"
+                  className={`form-control ${styles.inputs}`}
                   required // 添加必填
                 />
                 <MdLockOutline
                   className={`${styles['input-icon']}`}
                   size={22}
-                  style={{ color: '#E0B0FF' }}
+                  style={{ color: '#E0B0FF', cursor: 'pointer' }}
                 />
               </div>
               <div id="Error_message" className={`form-text text-white p-5`}>
-              {errors.message && <div className="error">{errors.message}</div>}              
+                {errors.message && (
+                  <div className="error">{errors.message}</div>
+                )}
               </div>
               <button
                 onClick={() => {
@@ -164,6 +169,5 @@ export default function LogIn(props) {
         }
       `}</style>
     </div>
-    
   )
 }
