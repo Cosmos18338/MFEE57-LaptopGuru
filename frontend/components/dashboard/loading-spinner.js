78ae2d2f722126cb123c616493e07e469a6620aa
Abby <aintluminate@gmail.com>
2024-11-18 11:28:55 +0800

    變更useLoader的全域樣式還有刪除登入註冊頁的英文

--- frontend/components/dashboard/loading-spinner.js
+++ frontend/components/dashboard/loading-spinner.js
@@ -1,10 +1,10 @@
 // components/dashboard/LoadingSpinner.js
 import React from 'react';
-// import { PacmanLoader } from "react-spinners";
+import { PacmanLoader } from "react-spinners";
 
-export const LoadingSpinner = ({ loading = true }) => {
-  console.log('LoadingSpinner rendered', { loading });
-  if (!loading) return null;
+export const LoadingSpinner = ({ show }) => {
+  console.log('LoadingSpinner rendered', {show});
+  if (!show) return null;
   
   return (
     <div className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center bg-black bg-opacity-25" style={{ zIndex: 9999 }}>
@@ -12,7 +12,7 @@ export const LoadingSpinner = ({ loading = true }) => {
         color="#d8d8d8"  // 你可以換成 "#e8e4e4" 或其他顏色
         // 灰色 #d8d8d8
         // 粉色 #f2dcdc
-        loading={loading}
+        // loading={loading}
         size={100}       // Pacman 的大小
         aria-label="Loading Spinner"
       />
